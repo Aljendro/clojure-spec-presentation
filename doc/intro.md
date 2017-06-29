80278508b668111321207f2c900c2dfef32d6f24
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2017-06-29 10:48:12 -0700

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-spec-presentation
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_spec_presentation/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-spec-presentation.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-spec-presentation.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
