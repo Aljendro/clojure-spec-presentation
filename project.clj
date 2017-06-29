1abac204b1c74c52ada39c6469c413de0bb6f2fd
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2017-06-29 12:21:39 -0700

    Added clojure.spec dependancies

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject clojure-spec-presentation "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "clojure.spec walkthrough"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.clojure/clojure.spec "0.1.123"]
+                 [org.clojure/test.check "0.9.0"]]
   :main ^:skip-aot clojure-spec-presentation.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
